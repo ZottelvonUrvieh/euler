f6d8ddc333c7ad05168d6bfae4233cba440d6317
zottelvonurvieh <s=nentergang@gmx.de>
2014-09-14 12:59:14 +0200

    solved

--- e001/src/mulp3and5.java
+++ e001/src/mulp3and5.java
@@ -1,16 +1,10 @@
-import java.util.*;
-
 public class mulp3and5 {
 	public static void main(String[] args){
-		List<Integer> array = new ArrayList<Integer>();
+		int sum = 0;
 		for (int i = 1; i < 1000; i++) {
 			if (i%3== 0 | i%5 == 0 )
-					array.add(i);
+					sum += i;
 			
-		}
-		int sum = 0;
-		for (Integer integer : array) {
-			sum += integer;
 		}
         System.out.println(sum);
    }
