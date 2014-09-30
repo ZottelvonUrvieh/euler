cb2daa967f6cd95af3921291b0fef00556d64391
zottelvonurvieh <s=nentergang@gmx.de>
2014-09-30 18:39:09 +0200

    starting 8th 7th works

---
+++ e007/src/nthPrime.java
@@ -0,0 +1,31 @@
+
+public class nthPrime {
+	public static void main(String[] args) {
+		System.out.println(nthPrim(10001));
+	}
+	public static int nthPrim(int n) {
+	    if (n < 2) return 2;
+	    if (n == 2) return 3;
+	    int limit, root, count = 1;
+	    limit = (int)(n*(Math.log(n) + Math.log(Math.log(n)))) + 3;
+	    root = (int)Math.sqrt(limit) + 1;
+	    limit = (limit-1)/2;
+	    root = root/2 - 1;
+	    boolean[] sieve = new boolean[limit];
+	    for(int i = 0; i < root; ++i) {
+	        if (!sieve[i]) {
+	            ++count;
+	            for(int j = 2*i*(i+3)+3, p = 2*i+3; j < limit; j += p) {
+	                sieve[j] = true;
+	            }
+	        }
+	    }
+	    int p;
+	    for(p = root; count < n; ++p) {
+	        if (!sieve[p]) {
+	            ++count;
+	        }
+	    }
+	    return 2*p+1;
+	}
+}
---
+++ e008/src/serializeStringToArray.java
@@ -0,0 +1,5 @@
+public class serializeStringToArray {
+	public static void main(String[] args) {
+		
+	}
+}
