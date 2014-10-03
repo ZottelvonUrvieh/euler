1f5fba00dafa6ea76e700577e8f749a9fae17f0c
zottelvonurvieh <s=nentergang@gmx.de>
2014-10-03 16:33:44 +0200

    initial commit problem 9

---
+++ e009/src/pythagoreanTriplet.java
@@ -0,0 +1,18 @@
+
+public class pythagoreanTriplet {
+	public static void main(String[] args) {
+		int product = 1000;
+		System.out.println(triples(product));
+	}
+
+	public static char[] triples(int product) {
+		int answer = 1;
+		for (int a = 1; a < 35){
+			for (int b = 1; b < 35){
+				for (int c = 1; c < 35)
+			}
+		}
+		return answer;
+	}
+
+}
